28e61f90d92bfe9cb5180dd61992633a9dc89d49
a_cavis <aacavis@gmail.com>
2015-05-11 12:03:12 -0600

    Reuploading site

---
+++ scripts/random-quote.js
@@ -0,0 +1,13 @@
+var r_text = [
+	["GRAPHIC DESIGN", "at Front Range", "This certificate program prepares you for entry-level positions in digital and graphics firms."],
+
+	["WEB DESIGN", "at Front Range", "This certificate program prepares you for entry-level positions with companies involved in web design. Students learn current web coding languages and techniques and create a website."],
+
+	["VIDEO", "at Front Range", "This certificate program prepares you for entry-level positions in companies involved in video production, editing, and designing."],
+
+	["ANIMATION", "at Front Range", "This certificate program prepares you for entry-level positions dealing with the production of interactive presentations, medical illustration, gaming, web animation, and cartooning."],
+
+]
+
+var i = Math.floor(4*Math.random())
+
